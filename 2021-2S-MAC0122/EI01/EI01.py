e9f83d9be9e1b0b9de753959c40931258632189a
Brenno Machado <brenno.pm@gmail.com>
2021-08-18 20:49:01 -0300

    alteraçoes em pastas e EI01

---
+++ 2021-2S-MAC0122/EI01/EI01.py
@@ -0,0 +1,38 @@
+# ===============================================================
+"""
+Ao incluir esse cabeçalho declaro que todas as partes originais
+desse exercício individual foram desenvolvidas e implementadas por
+mim e que portanto não constituem desonestidade acadêmica ou plágio.
+Declaro também que sou responsável por todas as cópias desse
+programa e que não distribui ou facilitei a sua distribuição.
+Estou ciente que os casos de plágio e desonestidade acadêmica
+serão tratados segundo os critérios divulgados na página da 
+disciplina.
+
+Entendo que exercícios sem esse cabeçalho devem receber nota zero
+e, ainda assim, poderão ser punidos por desonestidade acadêmica.    
+"""
+# ===============================================================
+
+def main():
+    ''' 
+    a função main é opcional. 
+    Caso desejar, inclua e deixe aqui os testes que você fez 
+    com as funções Hmaior e Hmenor.
+    '''
+    print("testes das funções")
+
+
+def Hmaior(n):
+    ''' escreva uma documentação para essa função
+    '''
+
+
+def Hmenor(n):
+    ''' escreva uma documentação para essa função
+    '''
+
+if __name__ == '__main__':
+    main()
+    n = int(input("\n Digite o número inteiro N para o cálculo do Harmonico de ordem N:"))
+    
