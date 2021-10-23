e548f57a28a621d77e402fc343da480838c2e4d6
Brenno Machado <brennomachado@tutanota.com>
2021-10-22 21:49:49 -0300

    Revert "Revert "EG16""
    This reverts commit 8357674f995d90d8cd3e4826284484ff2c3157d1.

--- EI16/tempos.py
+++ EI16/tempos.py
@@ -1,11 +1,11 @@
-from binomial import binomialI, binomialR, binomialRM
+from eg_binomial import binomialI, binomialR, binomialRM, binomialRC, binomialFAT 
 
 from timeit import default_timer as timer
 
 def main():
     
 
-    for n,k in [(0,0),(3,2),(5,1),(1,5),(4,2),(3,6),(6,3)]:
+    for n,k in [(0,0),(3,2),(5,1),(1,5),(4,2),(3,6),(160,30)]:
         start = timer()
         biR = binomialR(n,k)
         end = timer()
@@ -14,7 +14,17 @@ def main():
         start = timer()
         biI = binomialI(n,k)
         end = timer()
-        print(f"BinomialI ({n},{k}) = {biI}     => Tempo: {end-start}s\n")
+        print(f"BinomialI ({n},{k}) = {biI}     => Tempo: {end-start}s")
+
+        start = timer()
+        biR = binomialRC(n,k)
+        end = timer()
+        print(f"BinomialRC ({n},{k}) = {biR}     => Tempo: {end-start}s")
+
+        start = timer()
+        biI = binomialFAT(n,k)
+        end = timer()
+        print(f"BinomialFAT ({n},{k}) = {biI}     => Tempo: {end-start}s\n")
         
 
 if __name__ == '__main__':
